332b29aba6f7ad23897f6cf319828ea010e1d4d1
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-04-27 16:52:16 +0800

    add dark mode and something

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-const ilaw_active =  document.getElementById('ilaw');
-const ilaw_li =  document.querySelectorAll('.nav-links a');
+const ilaw_active = document.getElementById('ilaw');
+let ilaw_box = document.querySelector('.Box');
+let ilaw_box2 = document.querySelector('.Box-items');
+let active = true;
 
-ilaw_active.addEventListener('click' , () => {
-    document.body.classList.toggle('change-color');
-
-    ilaw_li.forEach(link => {
-        link.classList.toggle('nav-links-dark');
-    });
+ilaw_active.addEventListener('click', () => {
+    if (active){
+        document.body.style.backgroundColor = 'white';
+        ilaw_box.style.backgroundColor = '#03081a';
+        ilaw_box2.style.backgroundColor = '#03081a';
+        active = false;
+        console.log(active);
+    } else if (!active){
+        document.body.style.backgroundColor = '#03081a';
+        ilaw_box.style.backgroundColor = 'white';
+        ilaw_box2.style.backgroundColor = 'white';
+        active = true;
+        console.log(active);
+    }
 })
+
+/*
+
+    document.body.toggleAttribute(active = true);
+
+    if (active) {
+        document.body.classList.toggle('change-color');
+    }
+    console.log(active);
+
+*/
