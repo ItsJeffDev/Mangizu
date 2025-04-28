22474f2a12919a5d0637af4b7fbca78e481aa216
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-04-28 10:47:27 +0800

    add more

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const ilaw_active = document.getElementById('ilaw');
 let ilaw_box = document.querySelector('.Box');
-let ilaw_box2 = document.querySelector('.Box-items');
 let active = true;
 
 ilaw_active.addEventListener('click', () => {
     if (active){
         document.body.style.backgroundColor = 'white';
         ilaw_box.style.backgroundColor = '#03081a';
-        ilaw_box2.style.backgroundColor = '#03081a';
         active = false;
         console.log(active);
     } else if (!active){
         document.body.style.backgroundColor = '#03081a';
         ilaw_box.style.backgroundColor = 'white';
-        ilaw_box2.style.backgroundColor = 'white';
         active = true;
         console.log(active);
     }
